61ed0c12dbec432bebb508db52715a2a753814d7
Ramakrishna Boya <59126595+ramaboya@users.noreply.github.com>
2022-11-28 15:33:50 +0000

    Update Startup.cs

--- Application/src/RazorPagesTestSample/Startup.cs
+++ Application/src/RazorPagesTestSample/Startup.cs
@@ -10,7 +10,7 @@ namespace RazorPagesTestSample
     public class Startup
     {
         //comment to trigger build
-        //comment 3 adb ghy
+        //comment 3 adb ghy hyd
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("InMemoryDb"));
